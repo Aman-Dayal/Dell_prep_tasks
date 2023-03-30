4041e962c417525381ea8d8d8db861fd025dbe60
Aman Dayal <118193512+Aman-Dayal@users.noreply.github.com>
2023-03-30 22:18:17 +0530

    Add files via upload
    api call with an async task.

---
+++ my_api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import motor.motor_asyncio
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/my_api')
+async def my_api():
+    
+    user1 = {}
+    user1['Name'] = str(input("Enter your name: "))
+    user1['Phone no.'] = int(input("Enter your number: "))
+    user1['_id'] = input('Enter an id: ')
+    await myfnc(user1) 
+    return user1
+
+
+
+async def myfnc(user):
+    
+    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/")
+    db = client.get_database("my_api")
+    coll = db.get_collection('ist')
+    await coll.insert_one(user)
+
+import tracemalloc
+tracemalloc.start()
+  
+
+if __name__ == '__main__':
+    app.run(debug=True , port= 9000)
